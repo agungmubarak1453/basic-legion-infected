82387984959455f6ee29bb9ce815162473a9d239
Agung Mubarak <61371592+agungmubarak1453@users.noreply.github.com>
2025-11-29 17:32:18 +0700

    Chore/pull development  (#3)

---
+++ Assets/Internals/Modules/Game/Prefabs/Level Manager/LevelManager.cs
@@ -0,0 +1,40 @@
+using System;
+
+using UnityEngine;
+
+namespace BasicLegionInfected.Game
+{
+    public class LevelManager : MonoBehaviour
+    {
+        public void LoadLevel()
+        {
+            LoadEnvironment();
+            PlaceObjects();
+
+			throw new NotImplementedException();
+		}
+
+        private void LoadEnvironment()
+        {
+            throw new NotImplementedException();
+        }
+
+		private void PlaceObjects()
+        {
+            PlaceEnemy();
+            PlaceDoor();
+
+			throw new NotImplementedException();
+		}
+
+		private void PlaceEnemy()
+        {
+			throw new NotImplementedException();
+		}
+
+		private void PlaceDoor()
+        {
+			throw new NotImplementedException();
+		}
+    }
+}

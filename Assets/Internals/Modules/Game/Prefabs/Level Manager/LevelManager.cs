e9dea118f92b6095bf96a8f23134eeefa30d86d0
Agung Mubarak <agungmubarak1453@gmail.com>
2025-12-01 17:16:07 +0700

    Feat/add level generator (#7)
    * feat: add rooms generator
    
    * feat: use room generator in level manager

--- Assets/Internals/Modules/Game/Prefabs/Level Manager/LevelManager.cs
+++ Assets/Internals/Modules/Game/Prefabs/Level Manager/LevelManager.cs
@@ -1,40 +1,59 @@
-using System;
-
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 namespace BasicLegionInfected.Game
 {
-    public class LevelManager : MonoBehaviour
-    {
-        public void LoadLevel()
-        {
-            LoadEnvironment();
-            PlaceObjects();
+	public class LevelManager : MonoBehaviour
+	{
+		[SerializeField] private Tile _wallTile;
+		[SerializeField] private Tile _doorTile;
 
-			throw new NotImplementedException();
+		[SerializeField] private GameObject _personPrefab;
+
+
+		[SerializeField] private Tilemap _tilemap;
+
+		private RoomProceduralGenerator _roomProceduralGenerator;
+		private List<GameObject> _objects = new();
+
+		public void LoadLevel()
+		{
+			_roomProceduralGenerator = new(_tilemap, _wallTile, _doorTile);
+
+			GenerateEnvironment();
+			PlaceObjects();
 		}
 
-        private void LoadEnvironment()
-        {
-            throw new NotImplementedException();
-        }
+		private void GenerateEnvironment()
+		{
+			_roomProceduralGenerator.Generate();
+		}
 
 		private void PlaceObjects()
-        {
-            PlaceEnemy();
-            PlaceDoor();
+		{
+			foreach (GameObject _object in _objects)
+			{
+				Destroy(_object);
+			}
 
-			throw new NotImplementedException();
+			_objects.Clear();
+
+			PlacePerson();
 		}
 
-		private void PlaceEnemy()
-        {
-			throw new NotImplementedException();
-		}
+		private void PlacePerson()
+		{
+			foreach (RectInt room in _roomProceduralGenerator.Rooms)
+			{
+				Vector2 spawnPosition = room.center;
+				GameObject person = GameObject.Instantiate(_personPrefab, spawnPosition, Quaternion.identity);
 
-		private void PlaceDoor()
-        {
-			throw new NotImplementedException();
+				_objects.Add(person);
+			}
+
+			Debug.Log("Mock PlacePerson");
 		}
-    }
+	}
 }

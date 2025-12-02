54de3608185bbe5d2e2563c1ff92fd282dfa6553
agungmubarak1453 <agungmubarak1453@gmail.com>
2025-12-03 05:43:14 +0700

    feat: add Door script

---
+++ Assets/Internals/Modules/Environment/Prefabs/Door/Door.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace BasicLegionInfected.Environment
+{
+    public class Door : MonoBehaviour
+    {
+        [SerializeField] private Collider2D _collider;
+
+        public bool IsOpen { get => _collider.isTrigger; }
+
+        public UnityEvent OnOpen = new();
+        public UnityEvent OnClose = new();
+
+		private void Start()
+		{
+            Close();
+		}
+
+        public void Switch()
+        {
+            if (IsOpen)
+            {
+                Close();
+            }
+            else
+            {
+                Open();
+            }
+        }
+
+		public void Open()
+        {
+            _collider.isTrigger = true;
+
+            OnOpen.Invoke();
+        }
+
+        public void Close()
+        {
+            _collider.isTrigger = false;
+
+            OnClose.Invoke();
+        }
+    }
+}

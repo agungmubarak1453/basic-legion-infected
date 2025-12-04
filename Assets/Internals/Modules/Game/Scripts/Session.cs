411fc7d7955c4e00e0b5b4eb1e398a3ed59ec7cf
agungmubarak1453 <agungmubarak1453@gmail.com>
2025-12-04 19:08:00 +0700

    feat: add Infected Effect on game

--- Assets/Internals/Modules/Game/Scripts/Session.cs
+++ Assets/Internals/Modules/Game/Scripts/Session.cs
@@ -8,14 +8,38 @@ namespace BasicLegionInfected.Game
     {
         private LevelManager _levelManager;
 
-        public void Initialize(LevelManager levelManager)
-        {
+		private EffectData _infectedEffectData;
+        public int _infectedCount;
+
+		public void Initialize(
+            LevelManager levelManager, EffectData infectedEffectData, int infectedCount
+        ) {
             _levelManager = levelManager;
+            _infectedEffectData = infectedEffectData;
+            _infectedCount = infectedCount;
         }
 
         public void Start()
         {
             _levelManager.LoadLevel();
+
+            Person[] persons = _levelManager.GetComponentsInChildren<Person>();
+
+            int neeededInfectedCount = _infectedCount;
+            foreach (Person person in persons)
+            {
+                if (neeededInfectedCount > 0)
+                {
+                    EffectManager effectManager = person.GetComponentInChildren<EffectManager>();
+                    effectManager.AddEffect(_infectedEffectData);
+
+					neeededInfectedCount--;
+                }
+                else
+                {
+                    break;
+                }
+            }
 		}
 
         public void Close()
--- Assets/Internals/Modules/Game/Scripts/SessionManager.cs
+++ Assets/Internals/Modules/Game/Scripts/SessionManager.cs
@@ -4,12 +4,14 @@ namespace BasicLegionInfected.Game
 {
     public class SessionManager : MonoBehaviour
     {
+        [SerializeField] EffectData _infectedEffectData;
+
         public Session CreateGameSession()
         {
             LevelManager levelManager = FindObjectOfType<LevelManager>();
 
             Session newSession = gameObject.AddComponent<Session>();
-            newSession.Initialize(levelManager);
+            newSession.Initialize(levelManager, _infectedEffectData, 2);
 
             return newSession;
         }

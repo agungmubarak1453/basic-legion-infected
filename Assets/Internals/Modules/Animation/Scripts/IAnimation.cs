c868bc3157f44f521c0dc0406066a8fe5c9df95e
agungmubarak1453 <agungmubarak1453@gmail.com>
2025-12-13 06:16:14 +0700

    refactor: create animation base class

---
+++ Assets/Internals/Modules/Animation/Scripts/IAnimation.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BasicLegionInfected.Animation
+{
+    public interface IAnimation
+    {
+        void StartAnimation();
+    }
+}

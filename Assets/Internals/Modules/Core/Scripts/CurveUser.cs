516015f1f9037002434a4896a618041dc479c199
agungmubarak1453 <agungmubarak1453@gmail.com>
2025-12-13 18:02:54 +0700

    feat: add curve user

---
+++ Assets/Internals/Modules/Core/Scripts/CurveUser.cs
@@ -0,0 +1,71 @@
+using Codice.Client.BaseCommands;
+using System;
+
+using UnityEngine;
+
+namespace BasicLegionInfected.Core
+{
+    public class CurveUser
+    {
+        public AnimationCurve Curve;
+        public float DurationSeconds;
+        public bool IsLooping;
+
+        private bool _isPlaying;
+        public bool IsPlaying
+        {
+            get
+            {
+                if (!_isPlaying) return false;
+
+
+                if (IsLooping)
+                {
+                    return true;
+                }
+                else
+                {
+                    float timeElapsedSeconds = Time.time - StartTime;
+                    bool isFinished = timeElapsedSeconds > DurationSeconds;
+
+                    if (isFinished) _isPlaying = false;
+
+                    return _isPlaying;
+                }
+            }
+            private set
+            {
+                _isPlaying = value;
+            }
+        }
+        public float StartTime { get; private set; }
+
+        public CurveUser(AnimationCurve curve, float durationSeconds, bool isLooping)
+        {
+            Curve = curve;
+            IsLooping = isLooping;
+            DurationSeconds = durationSeconds;
+        }
+
+        public void Start()
+        {
+            StartTime = Time.time;
+
+            IsPlaying = true;
+        }
+
+        public float GetCurrentTimeValue()
+        {
+            if (IsPlaying)
+            {
+                float timeElapsedSeconds = Time.time - StartTime;
+                float normalizedTimeElapsedSeconds = Mathf.Clamp01(timeElapsedSeconds / DurationSeconds);
+                float value = Curve.Evaluate(normalizedTimeElapsedSeconds);
+
+                return value;
+            }
+
+            return 0f;
+        }
+    }
+}

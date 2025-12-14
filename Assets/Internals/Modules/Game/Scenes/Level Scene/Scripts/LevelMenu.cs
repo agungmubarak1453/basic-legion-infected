290e86c162326b31ec9a5d2c98ad30087646aba1
agungmubarak1453 <agungmubarak1453@gmail.com>
2025-12-14 21:57:30 +0700

    feat: add back to main and settings button for LevelMenu

--- Assets/Internals/Modules/Game/Scenes/Level Scene/Scripts/LevelMenu.cs
+++ Assets/Internals/Modules/Game/Scenes/Level Scene/Scripts/LevelMenu.cs
@@ -4,20 +4,56 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+using BasicLegionInfected.View;
+using Codice.Client.BaseCommands.CheckIn.Progress;
+
 namespace BasicLegionInfected.Game
 {
     public class LevelMenu : MonoBehaviour
     {
+        [SerializeField] private Menu _menu;
+        [SerializeField] private SceneManager _sceneManager;
+
         [SerializeField] private Button _repeatButton;
+        [SerializeField] private Button _settingsButton;
+        [SerializeField] private Button _backToMainButton;
 
         private void Awake()
         {
             _repeatButton.onClick.AddListener(OnRepeatButtonClicked);
+            _settingsButton.onClick.AddListener(OnSettingsButtonClicked);
+            _backToMainButton.onClick.AddListener(OnBackToMainButtonClicked);
         }
 
         private void OnRepeatButtonClicked()
         {
             RepeatGame();
+            _menu.Hide();
+        }
+
+        private void OnSettingsButtonClicked()
+        {
+            Menu settingsMenu = MenuManager.Instance.GetMenu("settings");
+            settingsMenu.OnHide.AddListener(OnSettingsMenuHide);
+
+            settingsMenu.Show();
+
+            _menu.Hide();
+        }
+
+        private void OnBackToMainButtonClicked()
+        {
+            GameManager.Instance.ExitGame();
+            _menu.Hide();
+            _sceneManager.LoadScene("Main Menu Scene");
+        }
+
+        private void OnSettingsMenuHide()
+        {
+            _menu.Show();
+
+            Menu settingsMenu = MenuManager.Instance.GetMenu("settings");
+            settingsMenu.OnHide.RemoveListener(OnSettingsMenuHide);
         }
 
         public void RepeatGame()

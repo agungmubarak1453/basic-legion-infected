7a8e849dd987f3696caa52248d10317f12ab3033
Agung Mubarak <agungmubarak1453@gmail.com>
2025-12-15 20:53:31 +0700

    Fix/fix webgl animation (#28)
    * feat: add rounder method for Vector3
    
    * fix: fix WebGL animation problem in Vector3

--- Assets/Internals/Modules/Utility/Scripts/Rounder.cs
+++ Assets/Internals/Modules/Utility/Scripts/Rounder.cs
@@ -13,5 +13,15 @@ namespace BasicLegionInfected.Utility
 		{
 			return value >= near - deviation && value <= near + deviation; 
 		}
+
+        public static bool IsNearVector3(Vector3 value, Vector3 near)
+        {
+			return IsNearVector3(value, near, 0.01f);
+        }
+
+        public static bool IsNearVector3(Vector3 value, Vector3 near, float deviation)
+		{
+			return Mathf.Abs(value.magnitude - near.magnitude) < deviation;
+		}
 	}
 }

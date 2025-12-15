b1968f090e04a9fe73a1313b4b038b31f7096baf
agungmubarak1453 <agungmubarak1453@gmail.com>
2025-12-15 14:09:23 +0700

    fix: change AudioClipCatalogue to use array items and divide Audio Source to two

--- Assets/Internals/Modules/Audio/Prefabs/Audio Manager/AudioManager.cs
+++ Assets/Internals/Modules/Audio/Prefabs/Audio Manager/AudioManager.cs
@@ -11,40 +11,42 @@ namespace BasicLegionInfected.Audio
     {
         [SerializeField] private AudioClipCatalogue _catalogue;
 
-        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private AudioSource _backgroundAudioSource;
+        [SerializeField] private AudioSource _effectAudioSource;
 
         public float BackgroundVolume = 0.5f;
         public float EffectVolume = 0.1f;
 
         public void PlayBackgroundAudio(string audioCode)
         {
-            _audioSource.Stop();
+            _backgroundAudioSource.Stop();
 
-            _audioSource.volume = BackgroundVolume;
+            _backgroundAudioSource.volume = BackgroundVolume;
 
-            AudioClip audioClip = _catalogue.GetItem(audioCode);
-            _audioSource.clip = audioClip;
+            AudioClip[] audioClips = _catalogue.GetItem(audioCode);
+            _backgroundAudioSource.clip = audioClips[Random.Range(0, audioClips.Length)]; // Random choose
 
-            _audioSource.loop = true;
+            _backgroundAudioSource.loop = true;
 
-            _audioSource.Play();
+            _backgroundAudioSource.Play();
         }
 
-        public void StopBackgroundAudio(string audioCode)
+        public void StopBackgroundAudio()
         {
-            _audioSource.Stop();
+            _backgroundAudioSource.Stop();
         }
 
         public void PlayEffectAudio(string audioCode)
         {
-            PlayOneShotAudio(audioCode, BackgroundVolume);
+            PlayOneShotAudio(audioCode, EffectVolume);
         }
 
         private void PlayOneShotAudio(string audioCode, float volumeScale)
         {
-            AudioClip audioClip = _catalogue.GetItem(audioCode);
+            AudioClip[] audioClips = _catalogue.GetItem(audioCode);
+            AudioClip audioClip = audioClips[Random.Range(0, audioClips.Length)];
 
-            _audioSource.PlayOneShot(audioClip, volumeScale);
+            _effectAudioSource.PlayOneShot(audioClip, volumeScale);
         }
     }
 }
--- Assets/Internals/Modules/Audio/Scripts/AudioClipCatalogue.cs
+++ Assets/Internals/Modules/Audio/Scripts/AudioClipCatalogue.cs
@@ -8,5 +8,5 @@ using BasicLegionInfected.Data;
 namespace BasicLegionInfected.Audio
 {
     [CreateAssetMenu(fileName = "Audio Clip Catalogue", menuName = "Basic Legion Infected/Audio/Audio Clip Catalogue")]
-    public class AudioClipCatalogue : ACatalogue<AudioClip> { }
+    public class AudioClipCatalogue : ACatalogue<AudioClip[]> { }
 }

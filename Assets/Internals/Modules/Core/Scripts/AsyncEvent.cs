43701618a3a9dc28342bfa02f7e7a1dc9a0f876d
agungmubarak1453 <agungmubarak1453@gmail.com>
2026-01-12 09:03:16 +0700

    feat: add async event

---
+++ Assets/Internals/Modules/Core/Scripts/AsyncEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicLegionInfected.Core
+{
+    public class AsyncEvent
+    {
+        private event Func<Task> _handler;
+
+        public void Add(Func<Task> handler) => _handler += handler;
+        public void Remove(Func<Task> handler) => _handler -= handler;
+
+        public async Task InvokeAsync()
+        {
+            Delegate[] invocationList = _handler?.GetInvocationList();
+            if (invocationList != null)
+            {
+                IEnumerable<Task> tasks = invocationList.Cast<Func<Task>>()
+                    .Select(function => function());
+                await Task.WhenAll(tasks);
+            }
+        }
+    }
+
+}
